ca1afdaa35bf77365b4ca2749bd94fb912b8db35
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2022-04-01 00:00:34 -0400

    starting chapter 4

---
+++ hedgehog/apps/binance_mock/lib/binance_mock.ex
@@ -0,0 +1,39 @@
+defmodule BinanceMock do
+  use GenServer
+
+  alias Decimal, as: D
+
+  require Logger
+
+  defmodule State do
+    defstruct order_books: %{}, subscriptions:[], fake_order_id:1
+  end
+
+  defmodule OrderBook do
+    defstruct buy_side:[], sell_side: [], historical:
+  end
+
+  def start_link(_args) do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def init(_args) do
+    {:ok, %State{}}
+  end
+
+  def get_exchange_info do
+    Binance.get_exchange_info()
+  end
+
+  def order_limit_buy(symbol, quantity, price, "GTC") do
+    order_limit(symbol, qunatity, price, "BUY")
+  end
+
+  def order_limit_sell(symbol, quantity, price, "GTC") do
+    order_limit(symbol, qunatity, price, "SELL")
+  end
+
+  defp order_limits(symbol, quantity, price, side) do
+    ## TODO
+  end
+end
