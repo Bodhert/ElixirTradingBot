759c056979ddfc8317835f153c0197980632c6db
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2023-12-21 21:59:21 -0500

    wip throug chapter 10

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,11 @@
 import Config
 
+config :naive, Naive.Repo,
+  database: "naive_repo",
+  username: "user",
+  password: "pass",
+  hostname: "localhost"
+
 config :logger,
   level: :debug
 
@@ -8,4 +14,11 @@ if File.exists?("config/secrets.exs") do
 end
 
 config :naive,
+  ecto_repos: [Naive.Repo],
   binance_client: BinanceMock
+
+config :naive, Naive.Repo,
+  database: "naive",
+  username: "postgres",
+  password: "hedgehogSecretPassword",
+  hostname: "localhost"
