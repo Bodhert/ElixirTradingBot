4aeb081dc55afb72c345d808ecd43ca084337b68
Alejandro Cordoba Bodhert <alejandro.cordoba.bodhert@gmail.com>
2025-01-03 13:48:29 -0500

    Chapter 19 (#21)
    * working through chapter 19
    
    * finished chapter 19
    
    * self code review against franton code

---
+++ apps/indicator/lib/indicator.ex
@@ -0,0 +1,11 @@
+defmodule Indicator do
+  @moduledoc """
+  Documentation for `Indicator`.
+  """
+  def aggregate_ohlcs(symbol) do
+    DynamicSupervisor.start_child(
+      Indicator.DynamicSupervisor,
+      {Indicator.Ohlc.Worker, symbol}
+    )
+  end
+end
---
+++ apps/indicator/lib/indicator/ohlc/worker.ex
@@ -0,0 +1,28 @@
+defmodule Indicator.Ohlc.Worker do
+  use GenServer
+  require Logger
+  alias Core.Struct.TradeEvent
+
+  @logger Application.compile_env(:core, :logger)
+  @pubsub_client Application.compile_env(:core, :pubsub_client)
+
+  def start_link(symbol) do
+    GenServer.start_link(__MODULE__, symbol)
+  end
+
+  def init(symbol) do
+    symbol = String.upcase(symbol)
+    @logger.debug("Initializing a new OHLC worker for #{symbol}")
+
+    @pubsub_client.subscribe(
+      Core.PubSub,
+      "TRADE_EVENTS:#{symbol}"
+    )
+
+    {:ok, symbol}
+  end
+
+  def handle_info(%TradeEvent{} = trade_event, ohlc) do
+    {:noreply, Indicator.Ohlc.process(ohlc, trade_event)}
+  end
+end
